2a2fcf81936c36d85e87a271ecc2b74e2a359580
unknown <anji.gd17@gmail.com>
2023-07-28 23:19:44 +0530

    Left side component part completed

--- src/app/Components/Left-component/left-box.component.ts
+++ src/app/Components/Left-component/left-box.component.ts
@@ -9,8 +9,9 @@ export class LeftComponent {
   // Add any logic or functionality related to the footer here if needed
   data = [
     { id: '#trd35464', daysAgo: '10', filesCount: '2' },
-    { id: '#trd35465', daysAgo: '10', filesCount: '2' },
-    { id: '#trd35466', daysAgo: '10', filesCount: '2' }
+    { id: '#fdc35545', daysAgo: '10', filesCount: '2' },
+    { id: '#jws46456', daysAgo: '10', filesCount: '2' },
+    { id: '#klm35062', daysAgo: '10', filesCount: '2' },
   ];
 
 }
